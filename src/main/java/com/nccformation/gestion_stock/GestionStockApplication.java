d50b677150f15ea7dc628cd857f0f74a3968f91c
noumbisdenccformation <noumbissiej@proton.me>
2025-06-15 05:40:08 +0100

    "Add User and Role entities, repositories, and UserDetailsService"

--- src/main/java/com/nccformation/gestion_stock/GestionStockApplication.java
+++ src/main/java/com/nccformation/gestion_stock/GestionStockApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GestionStockApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GestionStockApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GestionStockApplication.class, args);
+    }
 
 }
